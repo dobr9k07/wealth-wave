a7c05de29e9722aca88982ca9e35381961486ace
dobr9k <dobr9k07@gmail.com>
2024-07-05 13:07:53 +0300

    example svg

---
+++ components/SvgIcon.tsx
@@ -0,0 +1,20 @@
+import { IconId } from "@/enums/iconSpriteId";
+import { FC } from "react";
+import sprite from "@/public/sprite.svg";
+
+interface ISvgIconProps {
+  id?: string;
+  className: string;
+  size: string | number;
+  iconId: IconId;
+}
+
+const SvgIcon: FC<ISvgIconProps> = ({ id, className, iconId, size }) => {
+  return (
+    <svg id={id} className={className} width={size} height={size}>
+      <use href={sprite + iconId} />
+    </svg>
+  );
+};
+
+export default SvgIcon;
---
+++ enums/iconSpriteId.ts
@@ -0,0 +1,3 @@
+export enum IconId {
+  IconLogoAuth = "#icon-logo-auth",
+}
